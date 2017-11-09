a341dff2ec63756b3a29a6c2d98c218813255e3d
Christian <cmoehle@gmx.net>
2017-11-09 14:01:17 +0100

    my third commit

--- test.py
+++ test.py
@@ -3,6 +3,11 @@ import numpy as np
 #print('hello world!')
 
 def circum(r):
-    return 2*np.pi*r*r
+    return 2*np.pi*r
 
 print(circum(2))
+
+def area(r):
+    return np.pi*r*r
+
+print(area(3))
